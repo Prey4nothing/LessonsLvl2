d013e44c3015c66f5437b930b45f269b241c8d82
Prey4nothing <4fun92@Mail.ru>
2022-01-25 18:35:15 +0300

    Lesson1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,90 @@
+import market.Market;
+import person.customer.Customer;
+import person.seller.Seller;
+import product.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    //1) Seller, может добавлять, убирать продукты, выставлять стоимость
+    //2) Customer может покупать товар, может что-то желать
+    //3) Когда seller продает продукты, у него они отнимаются, но добавляются у customer
+    // кэш отнимается у customer, добавляется к seller
+    //4) Поиск товара про продавцу и продукту, либо по продукту
+
+    public static void main(String[] args) {
+        Market market = new Market();
+
+        Seller firstSeller = createFirstSeller();
+        Seller secondSeller = createSecondSeller();
+
+        market.addSeller(firstSeller);
+        market.addSeller(secondSeller);
+
+        Customer customer = createFirstCustomer();
+        customer.findProductOnMarket(market, "Алексей", "Ушаков");
+        customer.info();
+    }
+
+    private static Customer createFirstCustomer() {
+        Product firstProduct = new Product();
+        firstProduct.setName(MarketConstants.TOMATOES_PRODUCT_NAME);
+        firstProduct.setQuantity(3);
+
+        Product secondProduct = new Product();
+        secondProduct.setName(MarketConstants.CUCUMBER_PRODUCT_NAME);
+        secondProduct.setQuantity(2);
+
+        return new Customer(List.of(firstProduct, secondProduct), 50);
+    }
+
+    private static Seller createFirstSeller() {
+        Seller seller = new Seller();
+        seller.setName("Виталий");
+        seller.setLastName("Еремин");
+        seller.setCash(0);
+
+        Product firstProduct = new Product();
+        firstProduct.setName(MarketConstants.TOMATOES_PRODUCT_NAME);
+        firstProduct.setPrice(10);
+        firstProduct.setQuantity(3);
+
+        Product secondProduct = new Product();
+        secondProduct.setName(MarketConstants.CUCUMBER_PRODUCT_NAME);
+        secondProduct.setPrice(2);
+        secondProduct.setQuantity(100);
+
+        List<Product> products = new ArrayList<>();
+        products.add(firstProduct);
+        products.add(secondProduct);
+        seller.setProducts(products);
+
+        return seller;
+    }
+
+    private static Seller createSecondSeller() {
+        Seller seller = new Seller();
+        seller.setName("Алексей");
+        seller.setLastName("Ушаков");
+        seller.setCash(0);
+
+        Product firstProduct = new Product();
+        firstProduct.setName(MarketConstants.TOMATOES_PRODUCT_NAME);
+        firstProduct.setPrice(13);
+        firstProduct.setQuantity(40);
+
+        Product secondProduct = new Product();
+        secondProduct.setName(MarketConstants.CUCUMBER_PRODUCT_NAME);
+        secondProduct.setPrice(10);
+        secondProduct.setQuantity(12);
+
+        List<Product> products = new ArrayList<>();
+        products.add(firstProduct);
+        products.add(secondProduct);
+        seller.setProducts(products);
+
+        return seller;
+    }
+
 }
